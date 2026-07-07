37eb1cd9a7b52b609f10731b9788297dbfbf99df
nafiuw <121857811+nafiuw@users.noreply.github.com>
2026-07-07 15:38:13 -0500

    Solved: Input and Output (hackerrank)

---
+++ hackerrank/Practice/cpp-input-and-output.c
@@ -0,0 +1,7 @@
+// Problem: Input and Output
+// Platform: hackerrank
+// Language: cpp
+// URL: https://www.hackerrank.com/challenges/cpp-input-and-output/problem?isFullScreen=true
+// Solved on: 2026-07-07T20:38:12.488Z
+
+// source not captured - copy it from the editor manually
