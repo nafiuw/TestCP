bd1136caa63a1f310ba4a775918e21d4c4c21072
nafiuw <121857811+nafiuw@users.noreply.github.com>
2026-07-09 03:53:04 -0500

    Solved: Conditional Statements (hackerrank)

---
+++ hackerrank/Practice/c-tutorial-conditional-if-else.cpp
@@ -0,0 +1,7 @@
+// Problem: Conditional Statements
+// Platform: hackerrank
+// Language: cpp
+// URL: https://www.hackerrank.com/challenges/c-tutorial-conditional-if-else/problem?isFullScreen=true
+// Solved on: 2026-07-09T08:53:03.719Z
+
+// source not captured - copy it from the editor manually
