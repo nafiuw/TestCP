ec41fb5e59654f045039ea320ad9d52d325f62fe
nafiuw <121857811+nafiuw@users.noreply.github.com>
2026-07-09 03:15:42 -0500

    Solved: Two Sum (leetcode)

---
+++ leetcode/Easy/two-sum.cpp
@@ -0,0 +1,25 @@
+// Problem: Two Sum
+// Platform: leetcode
+// Rating/Difficulty: Easy
+// Language: cpp
+// URL: https://leetcode.com/problems/two-sum/
+// Solved on: 2026-07-09T08:15:41.307Z
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> numMap;
+        int n = nums.size();
+        for (int i = 0; i < n; i++) {
+            numMap[nums[i]] = i;
+        }
+        for (int i = 0; i < n; i++) {
+            int complement = target - nums[i];
+            if (numMap.count(complement) && numMap[complement] != i) {
+                return {i, numMap[complement]};
+            }
+        }
+
+        return {}; 
+    }
+};
