f3c5e45d441b3702b15cd245e06cd90e32632eef
nafiuw <121857811+nafiuw@users.noreply.github.com>
2026-07-11 05:22:10 -0500

    Solved: 2231A - Construct an Array (codeforces)

---
+++ codeforces/800-899/2231A.cpp
@@ -0,0 +1,9 @@
+// Problem: 2231A - Construct an Array
+// Platform: codeforces
+// Contest: Contest-2231
+// Rating/Difficulty: 800
+// Language: C++23 (GCC 14-64, msys2)
+// URL: https://codeforces.com/contest/2231/submission/2231
+// Solved on: 2026-07-11T10:22:08.928Z
+
+// source not captured automatically - open the submission page and copy it manually
