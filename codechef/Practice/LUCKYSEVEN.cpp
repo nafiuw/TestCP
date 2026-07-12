143d88788a13f11dc78c8fa72e07ebab249f98d3
nafiuw <121857811+nafiuw@users.noreply.github.com>
2026-07-12 15:42:38 +0600

    Solved: Welcome to the CodeChef AI Tutor (codechef)

---
+++ codechef/Practice/LUCKYSEVEN.cpp
@@ -0,0 +1,7 @@
+// Problem: Welcome to the CodeChef AI Tutor
+// Platform: codechef
+// Language: C++​
+// URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/LUCKYSEVEN
+// Solved on: 2026-07-12T09:42:37.371Z
+
+proceeding
