ae2162cd9dcbfbc825bae12a0827bf1624b50bc5
nafiuw <121857811+nafiuw@users.noreply.github.com>
2026-07-12 15:44:41 +0600

    Solved: Welcome to the CodeChef AI Tutor (codechef)

---
+++ codechef/Practice/CWC23QUALIF.cpp
@@ -0,0 +1,7 @@
+// Problem: Welcome to the CodeChef AI Tutor
+// Platform: codechef
+// Language: C++​
+// URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/CWC23QUALIF
+// Solved on: 2026-07-12T09:44:40.545Z
+
+3
