b8b136b398b850979cc3a2859bc68dfdc4304dd8
nafiuw <121857811+nafiuw@users.noreply.github.com>
2026-07-12 15:39:44 +0600

    Solved: Welcome to the CodeChef AI Tutor (codechef)

---
+++ codechef/Practice/DOUBLERENT.cpp
@@ -0,0 +1,7 @@
+// Problem: Welcome to the CodeChef AI Tutor
+// Platform: codechef
+// Language: C++​
+// URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DOUBLERENT
+// Solved on: 2026-07-12T09:39:43.989Z
+
+2
