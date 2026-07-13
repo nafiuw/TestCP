c66cd81674fad47aff5ac475843e9644ce6e1ec2
nafiuw <121857811+nafiuw@users.noreply.github.com>
2026-07-13 13:29:53 +0600

    Solved: Welcome to the CodeChef AI Tutor (codechef)

---
+++ codechef/Practice/TAXSAVING.cpp
@@ -0,0 +1,8 @@
+// Problem: Welcome to the CodeChef AI Tutor
+// Platform: codechef
+// Language: C++​
+// URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/TAXSAVING
+// Solved on: 2026-07-13T07:29:45.532Z
+
+	    cout << x - y <<'\n';
+
