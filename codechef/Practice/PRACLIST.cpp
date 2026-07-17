e708bb48209b0789b7a3d36d1da9b36f0b8ca3c9
nafiuw <121857811+nafiuw@users.noreply.github.com>
2026-07-17 17:08:12 +0600

    Solved: PRACLIST (codechef)

---
+++ codechef/Practice/PRACLIST.cpp
@@ -0,0 +1,15 @@
+// Problem: PRACLIST
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/PRACLIST
+// Solved on: 2026-07-17T11:08:11.519Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int x, y; cin >> x >> y;
+    cout << x - y <<endl;
+}
