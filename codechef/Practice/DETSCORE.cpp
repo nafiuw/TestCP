93d937b03f1087de1ad7d56397ec0d74f3ed352c
nafiuw <121857811+nafiuw@users.noreply.github.com>
2026-07-21 23:42:34 +0600

    Solved: DETSCORE (codechef)

---
+++ codechef/Practice/DETSCORE.cpp
@@ -0,0 +1,19 @@
+// Problem: DETSCORE
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/DETSCORE
+// Solved on: 2026-07-21T17:42:34.043Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t; cin >> t;
+	while (t--) {
+	    int x, n;
+	    cin >> x >> n;
+	    cout << (x/10) * n <<endl;
+	}
+
+}
