3ed0124d42850ae906965d96d4637fed75d2def6
nafiuw <121857811+nafiuw@users.noreply.github.com>
2026-07-25 15:41:23 +0600

    Solved: Odd or Even | Practice (geeksforgeeks)

---
+++ geeksforgeeks/Practice/odd-or-even3618.py
@@ -0,0 +1,31 @@
+// Problem: Odd or Even | Practice
+// Platform: geeksforgeeks
+// Language: unknown
+// Verdict: Accepted
+// URL: https://www.geeksforgeeks.org/problems/odd-or-even3618/1?page=1&difficulty=Basic&sortBy=submissions
+// Solved on: 2026-07-25T09:41:22.066Z
+
+class GfG {
+
+    public static boolean isEven(int n)
+    {
+        // finding remainder of n
+        int rem = n % 2;
+        if(rem == 0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+    
+    // Driver Code
+    public static void main(String[] args)
+    {
+        int n = 15;
+        if (isEven(n) == true)
+            System.out.print("true");
+        else
+            System.out.print("false");
+    }
+}
