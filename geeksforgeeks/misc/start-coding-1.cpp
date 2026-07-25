d2fc1614bab408d7873594d96b613aa6c33ae205
nafiuw <121857811+nafiuw@users.noreply.github.com>
2026-07-25 13:49:19 +0600

    Solved: Start Coding | Practice (geeksforgeeks)

---
+++ geeksforgeeks/misc/start-coding-1.cpp
@@ -0,0 +1,15 @@
+// Problem: Start Coding | Practice
+// Platform: geeksforgeeks
+// Language: unknown
+// Verdict: Accepted
+// URL: https://www.geeksforgeeks.org/problems/start-coding-1/1?page=1&category=CPP&sortBy=submissions
+// Solved on: 2026-07-25T07:49:18.790Z
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    // cout << "Hello World" << endl; // This line is commented
+    cout << "GeeksForGeeks" << endl; // This will be printed
+    return 0;
+}
