dbcf464b2633731db138d794e8f2effbafe8a2e9
nafiuw <121857811+nafiuw@users.noreply.github.com>
2026-07-25 14:02:16 +0600

    Solved: While Loop | Practice (geeksforgeeks)

---
+++ geeksforgeeks/misc/while-loop.py
@@ -0,0 +1,12 @@
+// Problem: While Loop | Practice
+// Platform: geeksforgeeks
+// Language: unknown
+// Verdict: Accepted
+// URL: https://www.geeksforgeeks.org/problems/while-loop/1?page=1&category=CPP&sortBy=submissions
+// Solved on: 2026-07-25T08:02:15.544Z
+
+class Solution:
+    def utility(self, x):
+        while x >= 0:
+            print(x, end=" ")
+            x -= 1
