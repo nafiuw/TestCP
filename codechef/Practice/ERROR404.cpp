ce16d295c80305be6b6eca2fef290ea3ac5677f3
nafiuw <121857811+nafiuw@users.noreply.github.com>
2026-07-25 13:42:15 +0600

    Solved: ERROR404 (codechef)

---
+++ codechef/Practice/ERROR404.cpp
@@ -0,0 +1,15 @@
+// Problem: ERROR404
+// Platform: codechef
+// Language: C++​
+// Verdict: Accepted
+// URL: https://www.codechef.com/practice/course/basic-programming-concepts/DIFF500/problems/ERROR404
+// Solved on: 2026-07-25T07:42:14.732Z
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int x; cin >> x;
+	cout << (x!=404 ? "FOUND" : "NOT FOUND");
+
+}
